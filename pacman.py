f8d4319c6a72caa43375b63bcc60ff3b04abc0c0
Vini5577 <ugopessoa@gmail.com>
2023-02-15 10:10:11 -0300

    configurando ghost

--- pacman.py
+++ pacman.py
@@ -1,19 +1,5 @@
-from ui import ui_print
+import random
 
-# @ -> our hero
-# G -> ghosts
-# P -> pills
-# . -> empety spaces
-# | and - -> walls;
-
-map = [
-    "|--------|",
-    "|G..|..G.|",
-    "|...PP...|",
-    "|G...@.|.|",
-    "|........|",
-    "|--------|"
-]
 
 def find_pacman(map):
     pacman_x = -1 
@@ -40,28 +26,58 @@ def move_pacman(map, next_pacman_x, next_pacman_y):
     everything_to_the_right = map[next_pacman_x][next_pacman_y+1:]
     map[next_pacman_x] = everything_to_the_left + "@" + everything_to_the_right
 
+# this function returns two booleans
+# the first indicates wheter the pressed key was a valid key
+# the second indicates whether the pacman is still alive
+# the third indicates wether the pacman won the game
 def play(map, key):
     next_x, next_y = next_position(map, key)
 
-    #if it is a invalid key
+    # if it is a invalid key
     is_an_invalid_key = next_x == -1 and next_y == -1
     if is_an_invalid_key:
-        return False
+        return False, True, False
 
-    is_within_borders = within_borders(map, next_x, next_y)
-    if not is_within_borders:
-        return False
+    # if it is not within borders
+    if not within_borders(map, next_x, next_y):
+        return False, True, False
+
+    # if it is a wall
+    if is_a_wall(map, next_x, next_y):
+        return False, True, False
+
+    is_a_ghost = map[next_x][next_y] == 'G'
+    if is_a_ghost:
+        return True, False, False
 
     move_pacman(map, next_x, next_y)
+
+    remaining_pills = total_pills(map)
+    if remaining_pills == 0:
+        return True, True, True
+    else:
+        return True, True, False
+
+def is_a_wall(map, next_x, next_y):
+    is_a_wall = map[next_x][next_y] == '|' or map[next_x][next_y] == '-'
+    return is_a_wall
 
 def within_borders(map, next_x, next_y):
     number_of_rows = len(map)
     x_is_valid = 0 <= next_x < number_of_rows
 
     number_of_columns = len(map[0])
-    y_is_valid = 0 <= next_y < number_of_columns 
+    y_is_valid = 0 <= next_y < number_of_columns
 
     return x_is_valid and y_is_valid
+
+def total_pills(map):
+    total = 0
+    for x in range(len(map)):
+        for y in range(len(map[x])):
+            if map[x][y] == 'P':
+                total = total  + 1
+    return total
 
 def next_position(map, key):
     x, y = find_pacman(map)
@@ -85,15 +101,40 @@ def next_position(map, key):
 
     return next_x, next_y
 
-ui_print(map)
-play(map, 'd')
-print("*********")
+def find_ghosts(map):
+    all_ghosts = []
+    for x in range(len(map)):
+        for y in range(len(map[x])):
+            if map[x][y] == 'G':
+                all_ghosts.append([x,y])
+    return all_ghosts
 
-ui_print(map)
-play(map, 'd')
-print("*********")
+def move_ghosts(map):
+    all_ghosts = find_ghosts(map)
+    for ghost in all_ghosts:
+        ghost_x = ghost[0]
+        ghost_y = ghost[1]
 
-ui_print(map)
-play(map, 'd')
-print("*********")
-ui_print(map)
+        possible_directions = [
+            [ghost_x, ghost_y + 1],
+            [ghost_x, ghost_y - 1],
+            [ghost_x - 1, ghost_y],
+            [ghost_x + 1, ghost_y]
+        ]
+
+        # select a random possible movement
+        # and get the x, y of the movement
+        random_number = random.randint(0, 3)
+        next_ghost_x = possible_directions[random_number][0]
+        next_ghost_y = possible_directions[random_number][1]
+
+        # move the ghost to the random position
+ 
+        everything_to_the_left = map[ghost_x][0:ghost_y]
+        everything_to_the_right = map[ghost_x][ghost_y + 1:]
+        map[ghost_x] = everything_to_the_left + "." + everything_to_the_right
+
+        # the new place has the pacman
+        everything_to_the_left = map[next_ghost_x][0:next_ghost_y]
+        everything_to_the_right = map[next_ghost_x][next_ghost_y + 1:]
+        map[next_ghost_x] = everything_to_the_left + "G" + everything_to_the_right
--- ui.py
+++ ui.py
@@ -3,5 +3,13 @@ def ui_print(map):
         for column in row:
             print(column, end='')
 
-        print()
-        
+        print('')
+
+def ui_key():        
+    return input()
+
+def ui_msg_lost():
+    print("Pacman died!")
+
+def ui_msg_win():
+    print("You won the game!")
